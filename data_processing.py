f76687bf30eb843250ecd18b22286d0f666c6138
Kyle Norris <r.kyle.norris@gmail.com>
2024-12-31 19:32:33 -0500

    worked on getting game outcomes table correct, each game is duplicated in the all_teams dataset

--- data_processing.py
+++ data_processing.py
@@ -13,6 +13,7 @@ conference_and_division_path = data_dir / "NHL_Conferences_Divisions_Teams.json"
 # set columns to be used in dataframes
 all_teams_columns = [
     'team',
+    'opposingTeam',
     'season',
     'gameId',
     'situation',
@@ -39,7 +40,7 @@ all_teams_columns = [
 ]
 
 shots_columns = [
-    'gameId',
+    'game_id',
     'homeTeamCode',
     'awayTeamCode',
     'season',
@@ -49,10 +50,12 @@ shots_columns = [
     'xPlayStopped',
     'shotType',
     'time',
+    'timeSinceFaceoff',
 ]
 
 # read in data
 all_teams_df = pd.read_csv(all_teams_path, usecols=all_teams_columns)
+all_teams_main_situation_df = all_teams_df[all_teams_df['situation'] == 'all'].copy()
 shots_22_df = pd.read_csv(shots_22_path, usecols=shots_columns)
 shots_07_21_df = pd.read_csv(shots_07_21_path, usecols=shots_columns)
 shots_df = pd.concat([shots_07_21_df, shots_22_df])
@@ -61,10 +64,18 @@ with open(conference_and_division_path) as f:
     conference_and_division_data = json.load(f)
 
 # concatenate season and gameID in shots df to match game id in all_teams_df
-shots_df['gameId'] = shots_df['season'].astype(str) + '0' + shots_df['gameId'].astype(str)
+shots_df['gameId'] = shots_df['season'].astype(str) + '0' + shots_df['game_id'].astype(str)
 
 # add game outcome column to all_teams_df
-all_teams_df['gameOutcome'] = all_teams_df.apply(lambda row: 'W' if row['goalsFor'] > row['goalsAgainst'] else ('L' if row['goalsFor'] < row['goalsAgainst'] else 'T'), axis=1)
+all_teams_main_situation_df['gameOutcome'] = all_teams_main_situation_df.apply(lambda row: 'W' if row['goalsFor'] > row['goalsAgainst'] else ('L' if row['goalsFor'] < row['goalsAgainst'] else 'T'), axis=1)
 
 # add OT column to shots_df
 shots_df['OT'] = shots_df.apply(lambda row: True if row['time'] > 3600 else False, axis=1)
+
+game_outcomes_car = all_teams_main_situation_df[all_teams_main_situation_df['team'] == 'CAR']
+
+carolina_games_win_loss = game_outcomes_car[['team', 'opposingTeam', 'season', 'gameId', 'gameOutcome']].groupby(['team', 'opposingTeam', 'season', 'gameId']).first().reset_index()
+
+carolina_games_win_loss.to_csv(data_dir / "carolina_games_win_loss.csv", index=False)
+
+# number of wins still not adding up but am close
